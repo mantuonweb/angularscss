3014b502306417a4f9970a5c428b008324c48cb0
Mantu Nigam <mantu.nigam@synapse.com>
2018-02-21 14:13:52 +0530

    shadow added

--- example/src/app/shadow/shadow/shadow.component.ts
+++ example/src/app/shadow/shadow/shadow.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShadowComponent implements OnInit {
   //https://www.w3schools.com/css/css3_shadows.asp
+  //https://www.w3schools.com/cssref/css3_pr_box-shadow.asp
   constructor() { }
 
   ngOnInit() {
